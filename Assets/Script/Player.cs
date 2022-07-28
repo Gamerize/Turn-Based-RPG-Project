8ab81677eb33351d9b735164c16764130c12aa23
Issac <gundam.issacwai@gmail.com>
2022-07-28 06:28:45 +0100

    Lesson 11 Inputs & Rigidbody Velocity

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D playerRigidbody;
+    [SerializeField] Rigidbody2D playerRigidbody;
+    [SerializeField] float m_Speed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float m_HorizontalMovement = Input.GetAxisRaw("Horizontal");
+        float m_VerticalMovement = Input.GetAxisRaw("Vertical");
+
+        playerRigidbody.velocity = new Vector2(m_HorizontalMovement * m_Speed, m_VerticalMovement * m_Speed);
     }
 }
